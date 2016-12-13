503db9b6f30a6f481d982a7420dc0d3bce9198f7
Em01 <emma_williams1@hotmail.co.uk>
2016-12-13 14:02:05 +0700

    functional components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBBgawIjznf7BRoZJtEPE6mZ46IwOnq6wE';
+
 // Create a new component. Component should produce some
 // HTML.
 
-const App = function() {
-  return <div>Hi!</div>;
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 //take component's generated HTML and put on the page ///(in the DOM).
 
-
+ReactDOM.render(
+  <App />,
+  document.querySelector('.container')
+);

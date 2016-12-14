883e88a2ed455171a5bdc6325fb90614bcd02f82
Em01 <emma_williams1@hotmail.co.uk>
2016-12-14 11:42:53 +0700

    props

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from'./components/video_list';
 const API_KEY = 'AIzaSyBBgawIjznf7BRoZJtEPE6mZ46IwOnq6wE';
 
 
@@ -21,6 +22,8 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
+        //passing prop videos to video list
       </div>
     );
   }

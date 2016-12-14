9148ee05f7dc461491fe67485c2cb5e2678bd8fd
Em01 <emma_williams1@hotmail.co.uk>
2016-12-14 11:31:22 +0700

    update index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyBBgawIjznf7BRoZJtEPE6mZ46IwOnq6wE';
 
-// Create a new component. Component should produce some
-// HTML.
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
-
 
 ReactDOM.render(
   <App />,
